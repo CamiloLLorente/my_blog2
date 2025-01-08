66c087e239e7be2b50a42aebe708ee865b335196
CamiloLLorente <camiloandresllorente@gmail.com>
2025-01-08 11:38:36 -0500

    Cambios generales

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,95 +22,100 @@ const Home = (): JSX.Element => {
   return (
     <div>
       <div className="relative">
-      <img
-        src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-        alt="Hero image"
-        className="w-full h-[700px] object-cover filter blur-[1.5px] brightness-50"
-      />
+        <img
+          src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+          alt="Hero image"
+          className="w-full h-[700px] object-cover filter blur-[1.5px] brightness-50"
+        />
       
-      <div className="absolute container mx-auto inset-0 flex flex-col items-start justify-start px-20 py-40 text-center text-white">
-        <div className=''>
-          <img
-            src={image}
-            alt="Hero image"
-            className="w-200 h-[200px] object-cover "
-          />
+        <div className="absolute container mx-auto inset-0 flex flex-col items-start justify-start px-20 py-40 text-center text-white">
+          <div className=''>
+            <img
+              src={image}
+              alt="Hero image"
+              className="w-200 h-[200px] object-cover "
+            />
         
-
+          </div>
+          <h1 className="text-white text-7xl font-bold  mb-2">
+            El camino, la verdad y la vida.
+          </h1>
+          <h2 className="text-[#FFA400] text-4xl font-bold">
+            Un viaje de fe y descubrimiento
+          </h2>
         </div>
-        <h1 className="text-white text-7xl font-bold  mb-2">
-          El camino, la verdad y la vida.
-        </h1>
-        <h2 className="text-[#FFA400] text-4xl font-bold">
-          Un viaje de fe y descubrimiento
-        </h2>
       </div>
-    </div>
 
+      <section className="bg-[#f2f2f2]  py-[100px]">
+        <div className="container mx-auto px-6 ">
+          <h2 className="text-3xl text-center text-[#FFA400] mb-4">Blogs</h2>
+          <h2 className="text-5xl font-bold text-center text-[#004694] mb-10">LO MÁS VISTO</h2>
 
-
-      <section className="mt-20 bg-[#E8E6E6] px-6 py-5 container mx-auto">
-        <h2 className="text-2xl font-bold text-[#004694] mb-4">Lo más reciente</h2>
-
-        <div className="grid gap-8 md:grid-cols-3">
-          {recentPosts.map((post) => (
-            <BlogPost
-              key={post.id}
-              id={post.id}
-              title={post.title}
-              description={post.description}
-              tags={post.tags}
-              date={post.date}
-              image={post.image}
-            />
-          ))}
-        </div>
-        <div className="flex justify-center g-4 mt-8">
-          <Link
-            to="/blog"
-          >
-            <button
-              type="submit"
-              className="bg-[#004694] text-white px-4 py-2 rounded hover:bg-[#003674] transition-colors flex "
+          <div className="grid gap-8 md:grid-cols-3">
+            {recentPosts.map((post) => (
+              <BlogPost
+                key={post.id}
+                id={post.id}
+                title={post.title}
+                description={post.description}
+                tags={post.tags}
+                date={post.date}
+                image={post.image}
+              />
+            ))}
+          </div>
+          <div className="flex justify-center g-4 mt-8">
+            <Link
+              to="/blog"
             >
-              <span className="mr-2">Ver mas</span> <span className="mt-[1px]"><ArrowRightCircle size={20} /></span>
-            </button>
-          </Link>
-          
-          
+              <button
+                type="submit"
+                className="bg-[#004694] text-white px-4 py-2 rounded hover:bg-[#003674] transition-colors flex "
+              >
+                <span className="mr-2">Ver mas</span> <span className="mt-[1px]"><ArrowRightCircle size={20} /></span>
+              </button>
+            </Link>
+            
+            
+          </div>
         </div>
       </section>
 
-      <section className="mb-12 bg-[#f2f2f2] px-6 py-12 container mx-auto">
-        <h2 className="text-2xl font-bold text-[#004694] mb-4">Lo más visto</h2>
-        <div className="grid gap-8 md:grid-cols-3">
-          {popularPosts.map((post) => (
-            <BlogPost
-              key={post.id}
-              id={post.id}
-              title={post.title}
-              description={post.description}
-              tags={post.tags}
-              date={post.date}
-              image={post.image}
-            />
-          ))}
-        </div>
-        <div className="flex justify-center g-4 mt-8">
-          <Link
-            to="/blog"
-          >
-            <button
-              type="submit"
-              className="bg-[#004694] text-white px-4 py-2 rounded hover:bg-[#003674] transition-colors flex "
+      <section className="bg-[#E8E6E6]  py-[100px]">
+        <div className="container mx-auto px-6 ">
+          <h2 className="text-3xl text-center text-[#FFA400] mb-4">Blogs</h2>
+          <h2 className="text-5xl font-bold text-center text-[#004694] mb-10">LO MÁS VISTO</h2>
+
+          <div className="grid gap-8 md:grid-cols-3">
+            {recentPosts.map((post) => (
+              <BlogPost
+                key={post.id}
+                id={post.id}
+                title={post.title}
+                description={post.description}
+                tags={post.tags}
+                date={post.date}
+                image={post.image}
+              />
+            ))}
+          </div>
+          <div className="flex justify-center g-4 mt-8">
+            <Link
+              to="/blog"
             >
-              <span className="mr-2">Ver mas</span> <span className="mt-[1px]"><ArrowRightCircle size={20} /></span>
-            </button>
-          </Link>
-          
-          
+              <button
+                type="submit"
+                className="bg-[#004694] text-white px-4 py-2 rounded hover:bg-[#003674] transition-colors flex "
+              >
+                <span className="mr-2">Ver mas</span> <span className="mt-[1px]"><ArrowRightCircle size={20} /></span>
+              </button>
+            </Link>
+            
+            
+          </div>
         </div>
       </section>
+
     </div>
   );
 };
--- src/pages/PostView.tsx
+++ src/pages/PostView.tsx
@@ -23,7 +23,7 @@ const PostView = () => {
   }
 
   return (
-    <div className="max-w-3xl mx-auto">
+    <div className="max-w-3xl mx-auto py-12">
       <img src={post.image} alt={post.title} className="w-full h-64 object-cover mb-8 rounded-lg" />
       <h1 className="text-4xl font-bold text-[#004694] mb-4">{post.title}</h1>
       <p className="text-gray-600 mb-8">{post.date}</p>
