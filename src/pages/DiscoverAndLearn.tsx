405419e5543f153a016c7d6d52fb6a19a413bd48
CamiloLLorente <camiloandresllorente@gmail.com>
2025-01-15 15:56:16 -0500

    Optimizando componente

--- src/pages/DiscoverAndLearn.tsx
+++ src/pages/DiscoverAndLearn.tsx
@@ -1,8 +1,43 @@
+import { useEffect, useState } from "react";
+import Search from "../components/Search";
+import { getPosts, Post } from "../data/posts";
+import Card from '../components/Card';
+
 const DiscoverAndLearn = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+    const [filteredPosts, setFilteredPosts] = useState<Post[]>([]);
+  
+    useEffect(() => {
+      const fetchPosts = async () => {
+        const fetchedPosts = await getPosts();
+        setPosts(fetchedPosts);
+        setFilteredPosts(fetchedPosts);
+      };
+      fetchPosts();
+    }, []);
+  const handleSearch = (query: string) => {
+    console.log('Buscando:', query);
+  }
+    
+
   return (
+    
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold text-[#004694] mb-8">Descubre y Aprende</h1>
-      <p>Bienvenido a la sección de Descubre y Aprende. Aquí encontrarás recursos y artículos para expandir tu conocimiento.</p>
+      <h1 className="text-3xl font-bold text-[#004694] mb-8 ">Blog</h1>
+      <Search onSearch={handleSearch} />
+      <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+        {filteredPosts.map((post) => (
+          <Card
+            key={post.id}
+            id={post.id}
+            title={post.title}
+            description={post.description}
+            tags={post.tags}
+            date={post.date}
+            image={post.image}
+          />
+        ))}
+      </div>
     </div>
   );
 };
