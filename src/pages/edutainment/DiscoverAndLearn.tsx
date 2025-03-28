72cabb8f77fbdc7a7ad89d6e55f0f58be33b6462
CamiloLLorente <camiloandresllorente@gmail.com>
2025-03-28 10:02:57 -0500

    Actualizando el componente DiscoverAndLearn para usar enlaces dinámicos en lugar de enlaces estáticos

--- src/pages/edutainment/DiscoverAndLearn.tsx
+++ src/pages/edutainment/DiscoverAndLearn.tsx
@@ -3,7 +3,6 @@ import { useEdutainment } from '../../hook/edutainment';
 
 const DiscoverAndLearn = () => {
   const { edutainmentFilter } = useEdutainment();
-
   return (
     <div className="container mx-auto px-4 py-20">
       <h1 className="text-3xl font-bold text-[#004694] mb-8 ">Aprende y Diviertete</h1>
@@ -16,7 +15,7 @@ const DiscoverAndLearn = () => {
             description={post.description}
             tags={post.tags}
             image={post.image}
-            link={`/blog/${post.id}`}
+            link={post.link}
           />
         ))}
       </div>
