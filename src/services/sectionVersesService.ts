4953bd51c29ab30f5df333db3cfbdcab8ad2c307
CamiloLLorente <camiloandresllorente@gmail.com>
2025-04-01 02:35:03 -0500

    Agregar función para obtener un versículo por ID en sectionVersesService

--- src/services/sectionVersesService.ts
+++ src/services/sectionVersesService.ts
@@ -1,6 +1,14 @@
-import { sectionsVerses } from "../data/data";
-import { SectionVerses } from "../data/interfaces";
+import { sectionsVerses, verses } from "../data/data";
+import { SectionVerses, Verse } from "../data/interfaces";
 
 export const getSectionsVerses = async (): Promise<SectionVerses[]> => {
     return sectionsVerses;
  };
+
+export const getVerseById = async (id: string): Promise<Verse[] | undefined> => {
+    const verse = verses[id];
+    if (!verse) {
+        throw new Error(`Verse with id ${id} not found`);
+    }   
+    return verse;
+};
