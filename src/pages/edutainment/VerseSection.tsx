dd289668f8e589c632c5570a8abe495b9846f51a
CamiloLLorente <camiloandresllorente@gmail.com>
2025-04-01 01:16:58 -0500

    Agregar el componente VerseSection para mostrar versículos bíblicos

---
+++ src/pages/edutainment/VerseSection.tsx
@@ -0,0 +1,21 @@
+
+const VerseSection = () => {
+
+  return (
+    <div className="container mx-auto px-4 py-20">
+      <h1 className="text-3xl font-bold text-[#004694] mb-8">
+        Seccion de versiculos biblicos
+      </h1>
+      <p>
+        Find inspiring verses to reflect on, strengthen your faith, and guide
+        your spiritual life.
+      </p>
+
+      <div className="justify-center justify-items-center">
+        section verse section
+      </div>
+    </div>
+  );
+};
+
+export default VerseSection;
