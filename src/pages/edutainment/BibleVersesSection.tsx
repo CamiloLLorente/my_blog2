97228eda9b58d6146145caf946b73aec9f9a0ad6
CamiloLLorente <camiloandresllorente@gmail.com>
2025-04-01 01:16:43 -0500

    Refactorizando BibleVersesSection

--- src/pages/edutainment/BibleVersesSection.tsx
+++ src/pages/edutainment/BibleVersesSection.tsx
@@ -1,44 +1,21 @@
+import { useEffect, useState } from "react";
 import SectionCard from "../../components/SectionCard";
+import { sectionsVerses } from "../../data/data";
 
 const BibleVersesSection = () => {
-  const sections = [
-    {
-      title: "Bible Verses Section",
-      description:
-        "Find inspiring verses to reflect on, strengthen your faith, and guide your spiritual life.",
-      color: "#ffaf00",
-    },
-    {
-      title: "Favorite Verses",
-      description:
-        "Find your favorite verses and keep learning from the word of God.",
-      color: "#bb02ff",
-    },
-    {
-      title: "Repasa tus versiculos",
-      description:
-        "Repasa tus versiculos favoritos y sigue aprendiendo de la palabra de Dios.",
-      color: "#da2268",
-    },
-    {
-      title: "Bible Verses Section",
-      description:
-        "Find inspiring verses to reflect on, strengthen your faith, and guide your spiritual life.",
-      color: "#ffaf00",
-    },
-    {
-      title: "Favorite Verses",
-      description:
-        "Find your favorite verses and keep learning from the word of God.",
-      color: "#bb02ff",
-    },
-    {
-      title: "Repasa tus versiculos",
-      description:
-        "Repasa tus versiculos favoritos y sigue aprendiendo de la palabra de Dios.",
-      color: "#da2268",
-    },
-  ];
+  const [sections, setSections] = useState<typeof sectionsVerses>([]);
+  useEffect(() => {
+    const fetchSections = async () => {
+      try {
+        const response = await sectionsVerses;
+        setSections(response);
+      } catch (error) {
+        console.error("Error fetching sections:", error);
+      }
+    };
+
+    fetchSections();
+  })
   return (
     <div className="container mx-auto px-4 py-20">
       <h1 className="text-3xl font-bold text-[#004694] mb-8">
@@ -48,7 +25,7 @@ const BibleVersesSection = () => {
         Find inspiring verses to reflect on, strengthen your faith, and guide
         your spiritual life.
       </p>
-
+      
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-8 justify-center justify-items-center">
         {sections.map((section, index) => (
           <SectionCard
@@ -56,11 +33,13 @@ const BibleVersesSection = () => {
             title={section.title}
             description={section.description}
             color={section.color}
+            url={`/versiculo/seccion/${section.slug}`}
+            slug={section.slug}
           />
         ))}
       </div>
     </div>
   );
-};
+ };
 
 export default BibleVersesSection;
