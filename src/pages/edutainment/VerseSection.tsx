337628713d1845351b7be0d1fc69ac4757098ad6
CamiloLLorente <camiloandresllorente@gmail.com>
2025-04-01 02:34:50 -0500

    Agregar lógica para obtener y mostrar versículos en VerseSection

--- src/pages/edutainment/VerseSection.tsx
+++ src/pages/edutainment/VerseSection.tsx
@@ -1,19 +1,43 @@
+import { useEffect, useState } from "react";
+import CardFavorite from "../../components/CardFavorite";
+import { getVerseById } from "../../services/sectionVersesService";
+import { Verse } from "../../data/interfaces";
+import { useParams } from "react-router-dom";
 
 const VerseSection = () => {
+  const [verses, setVerses] = useState<Verse[]>([]);
+
+  const { name } = useParams<{ name?: string }>(); // Hacemos que id sea opcional
+
+  useEffect(() => {
+    if (!name) return;
+
+    const getVerses = async () => {
+      try {
+        const response = await getVerseById(name);
+        setVerses(response || []);
+      } catch (error) {
+        console.error("Error fetching verses:", error);
+      }
+    };
+
+    getVerses();
+  }, [name]);
+  
 
   return (
     <div className="container mx-auto px-4 py-20">
       <h1 className="text-3xl font-bold text-[#004694] mb-8">
         Seccion de versiculos biblicos
       </h1>
-      <p>
-        Find inspiring verses to reflect on, strengthen your faith, and guide
-        your spiritual life.
-      </p>
-
-      <div className="justify-center justify-items-center">
-        section verse section
-      </div>
+      
+      {verses.map((verse) => (
+        <CardFavorite
+          key={verse.id}
+          title={verse.title}
+          description={verse.description}
+        />
+      ))}
     </div>
   );
 };
