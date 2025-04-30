311c9406a37a2dcd65d6295c62903880204eb871
CamiloLLorente <camiloandresllorente@gmail.com>
2025-04-29 19:11:20 -0500

    Agrega manejo de versos con almacenamiento local
    Renombra método para obtener versos por slug en lugar de ID.
    Añade almacenamiento en localStorage para versos consultados.
    Implementa función para marcar versos como favoritos.

--- src/services/sectionVersesService.ts
+++ src/services/sectionVersesService.ts
@@ -5,10 +5,29 @@ export const getSectionsVerses = async (): Promise<SectionVerses[]> => {
     return sectionsVerses;
  };
 
-export const getVerseById = async (id: string): Promise<Verse[] | undefined> => {
-    const verse = verses[id];
+export const getVerseByName = async (slug: string): Promise<Verse[] | undefined> => {
+    const storedVerse = localStorage.getItem(`verse_${slug}`);
+    if (storedVerse) {
+        return JSON.parse(storedVerse) as Verse[];
+    }
+
+    const verse = verses[slug];
     if (!verse) {
-        throw new Error(`Verse with id ${id} not found`);
-    }   
+        throw new Error(`Verse with slug ${slug} not found`);
+    }
+
+    localStorage.setItem(`verse_${slug}`, JSON.stringify(verse));
     return verse;
 };
+
+export const setFavorite = (slug: string, id: number,favorite: boolean) => {
+    const storedVerse = localStorage.getItem(`verse_${slug}`);
+    if (storedVerse) {
+        const versesArray = JSON.parse(storedVerse) as Verse[];
+        const verseIndex = versesArray.findIndex((v) => v.id === id);
+        if (verseIndex !== -1) {
+            versesArray[verseIndex].favorite = favorite;
+            localStorage.setItem(`verse_${slug}`, JSON.stringify(versesArray));
+        }
+    }
+}
