7929defc7979ce660bf63a38d278da6cdf152898
CamiloLLorente <camiloandresllorente@gmail.com>
2025-04-29 19:10:37 -0500

    Optimiza gestión de versículos en VerseSection
    Reemplaza lógica manual de obtención de datos con un hook reutilizable.
    Simplifica el manejo de favoritos al integrar toggleFavorite.
    Mejora la claridad y organización del código eliminando redundancias.

--- src/pages/edutainment/VerseSection.tsx
+++ src/pages/edutainment/VerseSection.tsx
@@ -1,41 +1,25 @@
-import { useEffect, useState } from "react";
-import CardFavorite from "../../components/CardFavorite";
-import { getVerseById } from "../../services/sectionVersesService";
-import { Verse } from "../../data/interfaces";
 import { useParams } from "react-router-dom";
+import CardFavorite from "../../components/CardFavorite";
+import useVerses from "../../hook/useVerses";
 
 const VerseSection = () => {
-  const [verses, setVerses] = useState<Verse[]>([]);
-
-  const { name } = useParams<{ name?: string }>(); // Hacemos que id sea opcional
-
-  useEffect(() => {
-    if (!name) return;
-
-    const getVerses = async () => {
-      try {
-        const response = await getVerseById(name);
-        setVerses(response || []);
-      } catch (error) {
-        console.error("Error fetching verses:", error);
-      }
-    };
-
-    getVerses();
-  }, [name]);
-  
+  const { name } = useParams<{ name?: string }>();
+  const { verses, toggleFavorite } = useVerses(name);
 
   return (
     <div className="container mx-auto px-4 py-20">
       <h1 className="text-3xl font-bold text-[#004694] mb-8">
         Seccion de versiculos biblicos
       </h1>
-      
       {verses.map((verse) => (
         <CardFavorite
           key={verse.id}
+          id={verse.id}
           title={verse.title}
+          favorite={verse.favorite}
           description={verse.description}
+          slug={name || ""}
+          onFavoriteUpdate={() => toggleFavorite(verse.id, verse.favorite)}
         />
       ))}
     </div>
