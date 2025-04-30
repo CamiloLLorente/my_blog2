a0ff19e10e96d44e07623b83841f58bf7734fb7b
CamiloLLorente <camiloandresllorente@gmail.com>
2025-04-29 19:07:19 -0500

    Agregar lógica para manejar la actualización de favoritos en el componente CardFavorite

--- src/components/CardFavorite/CardFavorite.tsx
+++ src/components/CardFavorite/CardFavorite.tsx
@@ -1,19 +1,37 @@
+import { PlusCircle } from "lucide-react";
 import React from 'react';
+import { setFavorite } from "../../services/sectionVersesService";
 
 interface CardFavoriteProps {
+    id: number;
     title: string;
     description: string;
+    favorite: boolean;
+    slug: string;
+    onFavoriteUpdate: () => void; // Nueva prop para notificar cambios
 }
 
-const CardFavorite: React.FC<CardFavoriteProps> = ({ title, description }) => {
+const CardFavorite: React.FC<CardFavoriteProps> = ({ id, title, description, favorite, slug, onFavoriteUpdate }) => {
+
+    const handleFavoriteClick = async () => {
+        await setFavorite(slug, id, !favorite);
+        onFavoriteUpdate(); // Notificar al componente padre
+    };
+
     return (
-        <div className="card-favorite">
-            <div className="card-favorite__content">
-                <h3 className="card-favorite__title">{title}</h3>
-                <p className="card-favorite__description">{description}</p> 
-            </div>
+        <div className="relative w-[500px] p-4 bg-white rounded-2xl shadow-lg border">
+          <button
+            onClick={handleFavoriteClick}
+            className="absolute top-2 right-2 text-gray-500 hover:text-red-500 transition"
+          >
+            {favorite ? <PlusCircle size={30} color='green'/> : <PlusCircle size={30}  color='grey'/>}
+          </button>
+          <div className="flex flex-col items-center">
+            <h3 className="mt-2 text-lg font-semibold">{title}</h3>
+            <p>{description}</p>
+          </div>
         </div>
-    );
+      );
 };
 
 export default CardFavorite;
